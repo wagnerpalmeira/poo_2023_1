3eff33018f5b25ca6953f96832741fa1d9961bb1
Wagner Palmeira <wagnerpalmeira.ti@gmail.com>
2023-05-23 21:35:40 -0300

    Adicionando Login

--- locadora/src/dao/UsuarioDao.java
+++ locadora/src/dao/UsuarioDao.java
@@ -3,6 +3,7 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import models.Usuario;
@@ -33,6 +34,20 @@ public class UsuarioDao {
 			System.out.println(e);
 			return false;
 		}
+	}
+	
+	public ResultSet autenticar(String username, String senha) {
+		String sql = "SELECT * FROM usuarios WHERE username=? AND senha=?";
+		try {
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setString(1, username);
+			stmt.setString(2, senha);
+			return stmt.executeQuery();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		return null;
 	}
 	
 	
--- locadora/src/services/LoginService.java
+++ locadora/src/services/LoginService.java
@@ -1,10 +1,22 @@
 package services;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import dao.UsuarioDao;
+
 public class LoginService implements ILoginService {
 	
+	private UsuarioDao usuarioDao = new UsuarioDao();
+	
 	public boolean autenticar(String login, String senha) {
-		if(login.equals("teste") && senha.equals("12345")) {
-			return true;
+		try {
+			ResultSet resultado = usuarioDao.autenticar(login, senha);
+			if(resultado.next()) {
+				return true;
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage().toString());
 		}
 		
 		return false;
